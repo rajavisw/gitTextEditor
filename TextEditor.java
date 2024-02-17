a64e5a45479a5db17b9903f963402cbf6cecf146
rajavisw <rajaviswanadhapalli12@gmail.com>
2024-02-17 14:23:15 +0530

    gitPush

---
+++ TextEditor.java
@@ -0,0 +1,157 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
+public class TextEditor implements ActionListener {
+    // Declaring properties of texteditor
+    JFrame frame;
+    JMenuBar menuBar;
+    JMenu file,edit;
+    JMenuItem newFile,openFile,saveFile;
+    JMenuItem cut,copy,paste,selectAll,close;
+    JTextArea textArea;
+    TextEditor(){
+
+        //Initialize a frame
+        frame=new JFrame();
+
+        //Initialize menubar
+        menuBar=new JMenuBar();
+
+        //Initialize Textarea
+        textArea =new JTextArea();
+
+        //Initialize menus
+        file=new JMenu("File");
+        edit=new JMenu("Edit");
+
+        //Initailize file menuItems
+        newFile=new JMenuItem("New Window");
+        openFile=new JMenuItem("Open File");
+        saveFile=new JMenuItem("Save File");
+        //add action listners to file menu
+        newFile.addActionListener(this);
+        openFile.addActionListener(this);
+        saveFile.addActionListener(this);
+        //add items to file
+        file.add(newFile);
+        file.add(openFile);
+        file.add(saveFile);
+
+        //Initialize edit menuItems
+        cut=new JMenuItem("Cut");
+        copy=new JMenuItem("Copy");
+        paste=new JMenuItem("Paste");
+        selectAll=new JMenuItem("Select All");
+        close=new JMenuItem("Close");
+        //add action listners to edit menu
+        cut.addActionListener(this);
+        copy.addActionListener(this);
+        paste.addActionListener(this);
+        selectAll.addActionListener(this);
+        close.addActionListener(this);
+        //add items to edit
+        edit.add(cut);
+        edit.add(copy);
+        edit.add(paste);
+        edit.add(selectAll);
+        edit.add(close);
+
+        //add menus to menuBar
+        menuBar.add(file);
+        menuBar.add(edit);
+
+        //set menuBar to frame
+        frame.setJMenuBar(menuBar);
+
+        JPanel panel=new JPanel();
+        panel.setBorder(new EmptyBorder(5,5,5,5));
+        panel.setLayout(new BorderLayout(0,0));
+        //add textArea
+        panel.add(textArea,BorderLayout.CENTER);
+        JScrollPane scrollPane=new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        panel.add(scrollPane);
+        frame.add(panel);
+        // set dimensions of frame
+        frame.setBounds(100,100,500,500);
+        frame.setTitle("Text Editor");
+        frame.setVisible(true);
+        frame.setLayout(null);
+    }
+    @Override
+    public void actionPerformed(ActionEvent actionEvent){
+        if(actionEvent.getSource()==cut){
+            //perform cut operation
+            textArea.cut();
+        }
+        if(actionEvent.getSource()==copy){
+            //perform copy operation
+            textArea.copy();
+        }
+        if(actionEvent.getSource()==paste){
+            //perform paste operation
+            textArea.paste();
+        }
+        if(actionEvent.getSource()==selectAll) {
+            //perform selectAll operation
+            textArea.selectAll();
+        }
+        if(actionEvent.getSource()==close){
+            //perform close operation
+            System.exit(0);
+        }
+        if(actionEvent.getSource()==openFile){
+            //open a file chooser
+            JFileChooser fileChooser=new JFileChooser("C:");
+
+            int chooseOption=fileChooser.showOpenDialog(null);
+            //If we have clicked an open button
+            if(chooseOption==JFileChooser.APPROVE_OPTION){
+                //getting selected file
+                File file=fileChooser.getSelectedFile();
+                //getting file path
+                String filePath=file.getPath();
+                try {
+                    FileReader fileReader=new FileReader(filePath);
+                    BufferedReader bufferedReader=new BufferedReader(fileReader);
+                    String intermediate="",output="";
+                    while((intermediate=bufferedReader.readLine())!=null){
+                        output+=intermediate+"\n";
+                    }
+                    //set the output string to text area
+                    textArea.setText(output);
+                }
+                catch (IOException ioException){
+                    ioException.printStackTrace();
+                }
+            }
+        }
+        if(actionEvent.getSource()==saveFile){
+            //Initialize file picker
+            JFileChooser fileChooser=new JFileChooser("C:");
+            //get choose option from file chooser
+            int chooseOption=fileChooser.showSaveDialog(null);
+            if(chooseOption==JFileChooser.APPROVE_OPTION){
+                File file=new File(fileChooser.getSelectedFile().getAbsolutePath()+".txt");
+                try {
+                    FileWriter fileWriter=new FileWriter(file);
+                    BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
+                    textArea.write(bufferedWriter);
+                    bufferedWriter.close();
+                }
+                catch (IOException ioException){
+                    ioException.printStackTrace();
+                }
+            }
+        }
+        if (actionEvent.getSource()==newFile){
+            TextEditor newTextEditor=new TextEditor();
+        }
+    }
+    public static void main(String[] args) {
+        TextEditor textEditor=new TextEditor();
+    }
+}
